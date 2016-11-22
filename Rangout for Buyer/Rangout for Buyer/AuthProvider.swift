685c9beb83d27e5090d832ad5d5d83619ec3009f
eiji <eiji.kawahira@gmail.com>
2016-11-22 12:18:53 -0200

    Auth implemented

---
+++ Rangout for Buyer/Rangout for Buyer/AuthProvider.swift
@@ -0,0 +1,9 @@
+//
+//  AuthProvider.swift
+//  Rangout for Buyer
+//
+//  Created by Eiji Kawahira on 22/11/16.
+//  Copyright © 2016 Eiji Kawahira. All rights reserved.
+//
+
+import Foundation
---
+++ Rangout for Vendor/Rangout for Vendor/AuthProvider.swift
@@ -0,0 +1,9 @@
+//
+//  AuthProvider.swift
+//  Rangout for Vendor
+//
+//  Created by Eiji Kawahira on 22/11/16.
+//  Copyright © 2016 Eiji Kawahira. All rights reserved.
+//
+
+import Foundation
